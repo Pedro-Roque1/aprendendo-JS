b93c53d837b588559a3f9a56a875572baf5b8038
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-14 10:18:26 -0300

    Conversão de Tipos
    Trabalhando com  concatenação, conversão implícita e explícita
    de strings e números, Not a Number(NaN).

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,9 @@
+console.log("Conversão de tipos");
+
+console.log ("ano : " + 2020);
+console.log ("2" + "2");//concatenação
+console.log (parseInt("2")+parseInt("2"));//forçar a converter o texto em número 
+console.log("7" / "2");
+console.log("Ricardo" / "2")//Not a Number->NaN
+console.log(6.5);
+console.log(6,5);
