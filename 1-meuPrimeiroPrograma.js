f548286b90264a77b1f625cbfd2544c7c24bfea2
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-14 09:41:28 -0300

    Primeiro programa em JS
    Trabalhando com variáveis e operadores aritméticos e os imprimindo
    na tela como resultado.

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,10 @@
+console.log("Meu primeiro programa : trabalhando com variaveis");
+
+const idade = 26;
+console.log (" idade: " + idade);
+console.log ("idade somada : " + (idade+2));
+console.log ("idade divida : " + (idade/2));
+console.log ("idade multiplicada : " + (idade*2));
+
+const idadeSomada = idade+2;
+console.log( "idade somada como variavel: " + idadeSomada);
