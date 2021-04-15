ecac9bff74fb1d9e0381c70688ce76f5d54b5c80
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-15 09:04:22 -0300

    Listagem em Js
    Usando forEach para listar dados e fazer cálculo do imc
    de cada paciente da tabela.

--- Projeto-web/js/principal.js
+++ Projeto-web/js/principal.js
@@ -4,24 +4,28 @@ var titulo = document.querySelector(".titulo");
 			//console.log(titulo.textContent);
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = document.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+pacientes.forEach(function(paciente) {
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var pesoValido =  peso < 1000 && peso > 0;
-var alturaValida = altura < 2.10 && altura > 0;
+    var pesoValido =  peso < 500 && peso > 0;
+    var alturaValida = altura < 2.10 && altura > 0;
 
-if (pesoValido == true && alturaValida == true){
-    var tdImc = paciente.querySelector(".info-imc");
-    var imc = peso/Math.pow(altura,2);
-    tdImc.textContent = imc;
-}else{
-    var tdImc = paciente.querySelector(".info-imc");
-    tdImc.textContent = "Peso ou altura inválidos";
-}
+    if (pesoValido && alturaValida) {
+        var tdImc = paciente.querySelector(".info-imc");
+        var imc = peso/Math.pow(altura,2);
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        var tdImc = paciente.querySelector(".info-imc");
+        paciente.classList.add("paciente-invalido");
+        tdImc.textContent = "Peso ou altura inválidos";
+    }
 
 
+});
