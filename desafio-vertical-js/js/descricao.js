378e4d1d9ccf389ff33c0ad4196b8df320b791dc
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-19 11:09:41 -0300

    Alteração na página de detalhe de Post
    A página foi alterada para ter elementos estáticos, onde é alterado
    somente o conteúdo dos elementos previamente criados no html.

--- desafio-vertical-js/js/descricao.js
+++ desafio-vertical-js/js/descricao.js
@@ -11,23 +11,9 @@ xhr.open("GET", `http://jsonplaceholder.typicode.com/posts/${idPost}`);
 xhr.addEventListener("load", function () {
     var resposta = xhr.responseText;
     var post = JSON.parse(resposta);
-
-    var listagem = document.querySelectorAll(".list-group-item");
-    listagem.forEach(function(lista,posicao) {
-        if (posicao == 0) {
-            var conteudoId = document.createElement("h6");
-            conteudoId.textContent = post.id;
-            lista.appendChild(conteudoId);
-        } else if(posicao == 1) {
-            var conteudoTitle = document.createElement("h6");
-            conteudoTitle.textContent = post.title;
-            lista.appendChild(conteudoTitle);
-        } else if(posicao == 2) {
-            var conteudoBody = document.createElement("h6");
-            conteudoBody.textContent = post.body;
-            lista.appendChild(conteudoBody);
-        }
-    });
+    document.querySelector(".id").textContent = post.id;
+    document.querySelector(".titulo").textContent = post.title;
+    document.querySelector(".desc").textContent = post.body;
 });
 
 xhr.send();
