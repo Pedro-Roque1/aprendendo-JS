48df3a21052a942db1f5ddae3872fc580dacb286
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-04-19 10:50:22 -0300

    Alteração da Url na descrição do Post
    Alterado a Url do endpoint da API para buscar diretamente
    em detalhamento do post específicado no link, anteriormente
    era capturado a lista de Posts.

--- desafio-vertical-js/js/descricao.js
+++ desafio-vertical-js/js/descricao.js
@@ -4,27 +4,27 @@
 const urlParams = new URLSearchParams(location.search);
 
 var idPost = urlParams.get('id');
-
 var xhr = new XMLHttpRequest();
 
-xhr.open("GET", `http://jsonplaceholder.typicode.com/posts?id=${idPost}`);
+xhr.open("GET", `http://jsonplaceholder.typicode.com/posts/${idPost}`);
 
 xhr.addEventListener("load", function () {
     var resposta = xhr.responseText;
-    var postArray = JSON.parse(resposta);
+    var post = JSON.parse(resposta);
+
     var listagem = document.querySelectorAll(".list-group-item");
     listagem.forEach(function(lista,posicao) {
         if (posicao == 0) {
             var conteudoId = document.createElement("h6");
-            conteudoId.textContent = postArray[0].id;
+            conteudoId.textContent = post.id;
             lista.appendChild(conteudoId);
         } else if(posicao == 1) {
             var conteudoTitle = document.createElement("h6");
-            conteudoTitle.textContent = postArray[0].title;
+            conteudoTitle.textContent = post.title;
             lista.appendChild(conteudoTitle);
         } else if(posicao == 2) {
             var conteudoBody = document.createElement("h6");
-            conteudoBody.textContent = postArray[0].body;
+            conteudoBody.textContent = post.body;
             lista.appendChild(conteudoBody);
         }
     });
